4d885af52c89b5a0859e5a9810c0863e2fbba635
MiKhin1115 <mikhinhantharsoe@uit.edu.mm>
2026-03-02 08:25:48 +0630

    App UI brief

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import Card from "./components/Card";
+import Form from './components/Form'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-full min-vh-100 d-flex justify-content-center align-item-center">
+      <div className="shadow-lg bg-light gap-4">
+        <Form/>
+        <Card/>
+      </div>
     </div>
+    
   );
 }
 
